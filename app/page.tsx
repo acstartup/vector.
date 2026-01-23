416a052b87eff6c48d80a8e2d26ccaff69c7dd64
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-22 19:30:01 -0500

    feat(home button): button to login

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ export default function Home() {
   return (
     <div>
       <div className="flex justify-start w-full"> {/* header */}
-        <a href="/" className="w-30 relative top-5 left-23">
+        <a href="/" className="w-30 relative top-6 left-23">
           <Image 
             src={vector} 
             alt="vector"
           ></Image>
+        </a>
+        <a href="/home" className="border relative bg-gradient-to-r from-violet-500 to-violet-600 w-30 h-11 text-center pt-2 rounded-md top-7.5 left-285 text-black font-[550]">Start for free.
         </a>
       </div>
     </div>
