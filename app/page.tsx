1e509c12f90fe232a3aee2c6097fd56219a88146
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-25 15:27:49 -0500

    feat(landing page): implemeneted heros, headers, subheaders, and images for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,55 @@
 import Image from "next/image";
 import vector from "../public/vector-full.png";
+import vectorDouble from "../public/vector-double4.png";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex justify-start w-full"> {/* header */}
-        <a href="/" className="w-30 relative top-6 left-23">
-          <Image 
-            src={vector} 
+    <div className="overflow-x-hidden overflow-y-hidden min-h-screen">
+      <div className="flex justify-between relative px-[100px] top-5 w-full h-40"> {/* header */}
+        <a 
+          className="w-30"
+          href="/">
+          <Image
+            src={vector}
             alt="vector"
           ></Image>
         </a>
-        <a href="/login" className="relative bg-gradient-to-r from-violet-500 to-violet-600 w-30 h-11 text-center pt-2 rounded-md top-7.5 left-285 text-black font-[550]">Start for free
-        </a> {/* login */}
-      </div>
-      <div className="flex relative justify-center top-45 w-220 mx-auto">
-        <h1 className="font-[550] text-7xl text-center">Quantify your daily grind to <span className="bg-gradient-to-r from-violet-500 to-violet-600 bg-clip-text text-transparent">make ambition reality.</span></h1>
-      </div>
-      <div className="flex relative justify-center top-50 w-240 mx-auto">
-        <h2 className="font-light text-white/80 text-[22px] text-center">Eliminate your feeling of falling behind with simple day-to-day tracking to reach your goals, from dominating your training to mastering your work and building a winning streak. </h2>
-      </div>
-      <div className="flex relative justify-center flex-col top-54 w-50 mx-auto">
-        <a href="/login" className="relative bg-gradient-to-r from-violet-500 to-violet-600 px-3 py-2 text-center rounded-md text-black font-[550]">Start your progress now
+        <a 
+          href="/login"
+          className="z-[10] relative text-center bg-white w-45 h-9 pt-1.5 px-3 py-2.25 rounded-md text-center text-black hover:opacity-93">
+          Get Started for Free
         </a>
-        <h3 className="text-md relative w-70 left-2 top-2 opacity-80">Vector is free for everyone.</h3>
+      </div>
+
+      <div className="relative top-25 px-[100px] w-230"> {/* Front Entrance */}
+        <span className="z-[10] relative text-white text-6xl font-light">Quantify your daily grind to</span>
+        <span className="z-[10] relative text-6xl right-0.25 font-semibold bg-gradient-to-r from-purple-500 via-pink-500 to-red-300 bg-clip-text text-transparent animate-gradient"> make ambition reality.</span>
+        <h3 className="z-[10] relative top-6 text-lg font-light opacity-70 w-148">
+          Eliminate your feeling of falling behind with simple day-to-day tracking to reach your goals, from dominating your training to mastering your work and building a winning streak.  
+        </h3>
+        <a 
+          href="/login"
+          className="relative top-14 border border-white w-45 h-20 py-2.25 px-3 rounded-md hover:bg-white/10">
+          Get Started for Free
+        </a>
+        <h2 className="relative top-20 opacity-70 font-light">
+          Vector is currently free for everyone.
+        </h2>
+
+        <div className="z-[0] absolute left-[735px] top-[-223px] w-[1000px] h-[800px]">
+          <Image
+            src={vectorDouble}
+            alt="full"
+            fill
+            className="object-contain"
+          ></Image>
+        </div>
+      </div>
+
+      <div className="z-[20] fixed flex justify-center left-1/2 -translate-x-1/2 bottom-3.5"> {/* Goal Tracking */}
+        <div className="py-1.5 px-3 backdrop-blur-xs font-normal text-white/60 rounded-full bg-white/2 border border-white/8">
+          100,000 goals reached with Vector
+        </div>
       </div>
     </div>
   );
