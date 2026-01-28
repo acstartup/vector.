94effbb6b7cabfd5906dab07689ec198b9f8b553
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-28 18:29:02 -0500

    feat(weight): externals

---
+++ app/weight/page.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import vector from "../../public/vector-full.png"
+import dashboard from "../../public/vector-dashboard.png"
+import work from "../../public/vector-work.png"
+import lift from "../../public/vector-lift.png"
+import todo from "../../public/vector-todo.png"
+import weight from "../../public/vector-weight.png"
+import setting from "../../public/vector-setting.png"
+import logout from "../../public/vector-logout.png"
+
+export default function Home() {
+    return (
+        <div className="px-[30px]">
+            <div className="flex justify-between relative top-5">
+                <a className="w-30">
+                    <Image
+                        src={vector}
+                        alt="Vector."
+                    ></Image>
+                </a>
+                <a className="bg-white/7 w-14 h-14 rounded-2xl">
+                </a>
+            </div>
+
+            <div className="flex flex-col gap-3 pt-[90px]">
+                <a
+                    href="../dashboard/"
+                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center pb-0.5 hover:bg-[#753B95]/60">
+                    <Image
+                        className="w-8.5 h-9"
+                        src={dashboard}
+                        alt="Dashboard"
+                    ></Image>
+                </a>
+                <a 
+                    href="../work/"
+                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                    <Image
+                        className="w-9 h-9"
+                        src={work}
+                        alt="Work"
+                    ></Image>
+                </a>
+                <a 
+                    href="../lift/"
+                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                    <Image
+                        className="w-9 h-9"
+                        src={lift}
+                        alt="Lift"
+                    ></Image>
+                </a>
+                <a 
+                    href="../todo/"
+                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                    <Image
+                        className="w-9 h-9"
+                        src={todo}
+                        alt="todo"
+                    ></Image>
+                </a>
+                <a 
+                    href="/weight/"
+                    className="bg-[#753B95] w-14 h-14 rounded-2xl grid place-items-center">
+                    <Image
+                        className="w-9 h-9"
+                        src={weight}
+                        alt="weight"
+                    ></Image>
+                </a>
+                <div className="flex flex-col pt-47 gap-3">
+                    <a 
+                        href="../setting/"
+                        className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center pl-1 hover:bg-[#753B95]/60">
+                        <Image
+                            className="w-9 h-9"
+                            src={setting}
+                            alt="setting"
+                        ></Image>
+                    </a>
+                    <a className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                        <Image
+                            className="w-9 h-9"
+                            src={logout}
+                            alt="logout"
+                        ></Image>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
