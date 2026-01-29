caa91fffcc2fb757b25c49ccb1d768c4d1a18bbb
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-29 17:10:53 -0500

    feat(settings): enternals with account and security header and sub-header

--- app/setting/page.tsx
+++ app/setting/page.tsx
@@ -7,11 +7,13 @@ import todo from "../../public/vector-todo.png"
 import weight from "../../public/vector-weight.png"
 import setting from "../../public/vector-setting.png"
 import logout from "../../public/vector-logout.png"
+import personal from "../../public/vector-personal-setting.png"
+import security from "../../public/vector-privacy-setting.png"
 
 export default function Home() {
     return (
-        <div className="px-[30px]">
-            <div className="flex justify-between relative top-5">
+        <div className="flex flex-col">
+            <div className="flex justify-between relative top-5 px-[30px]">
                 <a className="w-30">
                     <Image
                         src={vector}
@@ -22,69 +24,100 @@ export default function Home() {
                 </a>
             </div>
 
-            <div className="flex flex-col gap-3 pt-[90px]">
-                <a
-                    href="../dashboard/"
-                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center pb-0.5 hover:bg-[#753B95]/60">
-                    <Image
-                        className="w-8.5 h-9"
-                        src={dashboard}
-                        alt="Dashboard"
-                    ></Image>
-                </a>
-                <a 
-                    href="../work/"
-                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
-                    <Image
-                        className="w-9 h-9"
-                        src={work}
-                        alt="Work"
-                    ></Image>
-                </a>
-                <a 
-                    href="../lift/"
-                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
-                    <Image
-                        className="w-9 h-9"
-                        src={lift}
-                        alt="Lift"
-                    ></Image>
-                </a>
-                <a 
-                    href="../todo/"
-                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
-                    <Image
-                        className="w-9 h-9"
-                        src={todo}
-                        alt="Todo"
-                    ></Image>
-                </a>
-                <a 
-                    href="../weight/"
-                    className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
-                    <Image
-                        className="w-9 h-9"
-                        src={weight}
-                        alt="Weight"
-                    ></Image>
-                </a>
-                <div className="flex flex-col pt-47 gap-3">
+            <div className="flex">
+                <div className="flex flex-col gap-3 px-[30px] pt-[90px]">
+                    <a
+                        href="../dashboard/"
+                        className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center pb-0.5 hover:bg-[#753B95]/60">
+                        <Image
+                            className="w-8.5 h-9"
+                            src={dashboard}
+                            alt="Dashboard"
+                        ></Image>
+                    </a>
                     <a 
-                        href="/setting/"
-                        className="bg-[#753B95] w-14 h-14 rounded-2xl grid place-items-center pl-1">
+                        href="../work/"
+                        className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
                         <Image
                             className="w-9 h-9"
-                            src={setting}
-                            alt="Setting"
+                            src={work}
+                            alt="Work"
                         ></Image>
                     </a>
-                    <a className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                    <a 
+                        href="../lift/"
+                        className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
                         <Image
                             className="w-9 h-9"
-                            src={logout}
-                            alt="Logout"
+                            src={lift}
+                            alt="Lift"
                         ></Image>
                     </a>
+                    <a
+                        href="../todo/" 
+                        className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                        <Image
+                            className="w-9 h-9"
+                            src={todo}
+                            alt="todo"
+                        ></Image>
+                    </a>
+                    <a 
+                        href="../weight/"
+                        className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                        <Image
+                            className="w-9 h-9"
+                            src={weight}
+                            alt="weight"
+                        ></Image>
+                    </a>
+                    <div className="flex flex-col pt-47 gap-3">
+                        <a 
+                            href="/setting/"
+                            className="bg-[#753B95] w-14 h-14 rounded-2xl grid place-items-center pl-1">
+                            <Image
+                                className="w-9 h-9"
+                                src={setting}
+                                alt="setting"
+                            ></Image>
+                        </a>
+                        <a className="bg-white/7 w-14 h-14 rounded-2xl grid place-items-center hover:bg-[#753B95]/60">
+                            <Image
+                                className="w-9 h-9"
+                                src={logout}
+                                alt="logout"
+                            ></Image>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="flex flex-row gap-4 mt-[90px] ml-[65px]">
+                    <div className="flex w-98 h-23 rounded-3xl hover:bg-white/7">
+                        <div className="flex flex-row bg-white/7 ml-[15px] mt-[10.5px] rounded-2xl w-17.5 h-17.5">
+                            <Image
+                                className="flex mt-[11px] ml-[11px] w-12 h-12"
+                                src={personal}
+                                alt="personal"
+                            ></Image>
+                        </div>
+                        <div className="flex flex-col">
+                            <h1 className="mt-[13px] ml-[15px] w-40 text-md font-bold">Account</h1>
+                            <h2 className=" ml-[15px] w-70 text-sm">View and manage your profile picture and display name.</h2>
+                        </div>
+                    </div>
+                    <div className="flex w-98 h-23 rounded-3xl hover:bg-white/7">
+                        <div className="flex flex-row bg-white/7 ml-[15px] mt-[10.5px] rounded-2xl w-17.5 h-17.5">
+                            <Image
+                                className="flex mt-[14px] ml-[13.25px] w-10.75 h-10.75"
+                                src={security}
+                                alt="personal"
+                            ></Image>
+                        </div>
+                        <div className="flex flex-col">
+                            <h1 className="mt-[13px] ml-[15px] w-40 text-md font-bold">Security</h1>
+                            <h2 className=" ml-[15px] w-70 text-sm">Change and manage your account password to keep your account secure.</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
